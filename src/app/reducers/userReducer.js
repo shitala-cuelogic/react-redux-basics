3a2714921a346aa50338fc5b84a52e3315a23692
shitala-cuelogic <shital.agarwal@cuelogic.co.in>
2017-04-10 18:03:16 +0530

    Handling async functionality using promises

--- src/app/reducers/userReducer.js
+++ src/app/reducers/userReducer.js
@@ -3,7 +3,7 @@ const userReducer = (state = {
   age: 1
 }, action) => {
   switch (action.type) {
-    case "SET_NAME":
+    case "SET_NAME_FULFILLED": //this name is required as we have used promise package for async functionality
       state = {
         ...state,
         name: action.payload
--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import logger from "redux-logger";
 import {createStore, combineReducers, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 
+import promise from "redux-promise-middleware";
 import mathReducer from "./reducers/mathReducer";
 import userReducer from "./reducers/userReducer";
 
@@ -9,4 +10,4 @@ import userReducer from "./reducers/userReducer";
 export default createStore(combineReducers({
   math: mathReducer, //As per ES5
   userReducer //As per ES6
-}), {}, applyMiddleware(logger, thunk));
+}), {}, applyMiddleware(logger, thunk, promise()));
